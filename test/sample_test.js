c4b2ad86b4279e16e1ce060f6aec5988c91722c3
James <jordunjames@gmail.com>
2019-08-02 10:29:46 +0100

    added sample test

---
+++ test/sample_test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('A functionality', () => {
+    describe('inner functionality', () => {
+
+        it('should pass', () => {
+            assert(true);
+        })
+    })
+});
